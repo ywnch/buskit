e3f247ab374c1097ad1b17681684b60c49eaf107
Yuwen Chang <dd20085@gmail.com>
2018-04-03 17:53:21 -0400

    major update on functions

--- README.md
+++ README.md
@@ -6,10 +6,35 @@ Author: Yuwen Chang (M.S. at NYU CUSP 2018)
 
 Instructor: Joseph Chow
 
+## Currently Available Functions
+
+### bus_data: Get bus data
+
+Manually download the [script](https://github.com/ywnch/BusSimulator/blob/master/fetchbus.py) or clone the repo and import the functions to your Jupyter notebook or run it on command lines as the example below:
+
+```python
+git clone https://github.com/ywnch/BusSimulator.git
+# fetch bus data for route B54 for 1 minute (once per 30 secs)
+python fetchbus.py $MTAAPIKEY "B54" 1
+```
+
+### plot_tsd: Plot time-space diagram
+
+Import the functions and simply plot the dataframe as downloaded using `bus_data`.
+
+```python
+from fetchbus import plot_tsd
+# read the downloaded data
+df = pd.read_csv("B54-Tue-180403-170417-5.csv")
+# plot df starting from minute 1 to minute 3 and save it as TSD.png
+plot_tsd(df, start_min=1, end_min=3, save=True, fname='TSD')
+```
+
 ## Objectives
 
-1. Construct a simple environment with GUI that allow one to simulate a given NYC bus route easily with essential variables.
-2. Implement different bus bunching (BB) control strategies and evaluate their performances using real-world data.
+1. Construct a simple environment with GUI that allow all users to simulate a given NYC bus route easily with essential variables. (May be expanded to other cities given a standard processed data feed, specifically GTFS and SIRI.)
+2. For service operators: implement different bus bunching (BB) control strategies and evaluate their performances using real-world data.
+3. For bus riders: signal alerts for unreliable service window along the route
 
 ## Workflow
 
@@ -29,15 +54,20 @@ Instructor: Joseph Chow
 - Andres, M., & Nair, R. (2017). A predictive-control framework toaddress bus bunching. *Transportation Research Part B: Methodological*, *104*,123-148.
 - Bartholdi III, J. J., & Eisenstein, D. D. (2012). Aself-coördinating bus route to resist bus bunching. *TransportationResearch Part B: Methodological*, *46*(4), 481-491.
 - Camps, J. M., & Romeu, M. E. (2016). Headway Adherence. Detectionand Reduction of the Bus Bunching Effect. In *European TransportConference 2016Association for European Transport (AET)*.
+- Carreón, G., Gershenson, C., & Pineda, L. A. (2017). Improvingpublic transportation systems with self-organization: A headway-based model andregulation of passenger alighting and boarding. *PLoS one*, *12*(12),e0190100.
 - Daganzo, C. F. (2009). A headway-based approach to eliminate busbunching: Systematic analysis and comparisons. *Transportation ResearchPart B: Methodological*, *43*(10), 913-921.
+- dos Santos Silva, F. A. (2017). Comparing Bus Travel Time PredictionUsing AVL and Smart Card Data.
 - Feng, W., & Figliozzi, M. (2011a, January). Using archivedAVL/APC bus data to identify spatial-temporal causes of bus bunching. In *Proceedingsof the 90th Annual Meeting of the Transportation Research Board, Washington,DC, USA* (pp. 23-27).
 - Feng, W., & Figliozzi, M. (2011b). Empirical findings of bus bunchingdistributions and attributes using archived AVL/APC bus data. In *ICCTP2011: Towards Sustainable Transportation Systems* (pp. 4330-4341).
+- Kurkcu, A., Miranda, F., Ozbay, K., & Silva, C. T. (2017, June). Data visualization tool for monitoring transit operation and performance. In *Models and Technologies for Intelligent Transportation Systems (MT-ITS), 2017 5th IEEE International Conference on*(pp. 598-603). IEEE.
 - Luo, X., Liu, S., Jin, P. J., Jiang, X., & Ding, H. (2017). Aconnected-vehicle-based dynamic control model for managing the bus bunchingproblem with capacity constraints. *Transportation Planning andTechnology*, *40*(6), 722-740.
 - Mendes-Moreira, J., Jorge, A. M., de Sousa, J. F., & Soares, C.(2012). Comparing state-of-the-art regression methods for long term travel timeprediction. *Intelligent Data Analysis*, *16*(3), 427-449.
 - Moreira-Matias, L., Cats, O., Gama, J., Mendes-Moreira, J., & deSousa, J. F. (2016). An online learning approach to eliminate Bus Bunching inreal-time. *Applied Soft Computing*, *47*, 460-482.
 - Moreira-Matias, L., Ferreira, C., Gama, J., Mendes-Moreira, J., &de Sousa, J. F. (2012, July). Bus bunching detection by mining sequences ofheadway deviations. In *Industrial Conference on Data Mining*(pp.77-91). Springer, Berlin, Heidelberg.
 - Moreira-Matias, L., Gama, J., Mendes-Moreira, J., & de Sousa, J.F. (2014, October). An incremental probabilistic model to predict bus bunchingin real-time. In *International Symposium on Intelligent Data Analysis* (pp.227-238). Springer, Cham.
+- Pekel, E., & Soner Kara, S. (2017). A Comprehensive Review forArtificial Neural Network Application to Public Transportation. *Sigma:Journal of Engineering & Natural Sciences/Mühendislik ve Fen BilimleriDergisi*, *35*(1).
 - Pilachowski, J. M. (2009). *An approach to reducing busbunching*. University of California, Berkeley.
+- Yu, H., Wu, Z., Chen, D., & Ma, X. (2017). ProbabilisticPrediction of Bus Headway Using Relevance Vector Machine Regression. *IEEETransactions on Intelligent Transportation Systems*, *18*(7),1772-1781.
 
 ## Updates
 
--- fetchbus.py
+++ fetchbus.py
@@ -1,12 +1,12 @@
 # Author: Yuwen Chang, NYU CUSP
-# Last Updated: 2018/03/17
+# Last Updated: 2018/04/03
 ##############################
 # Code written for Bus Simulator
 # https://github.com/ywnch/BusSimulator
 ##############################
 # put MTA API key, bus route, bus direction, and duration as input arguments:
 # i.e. run the code as:
-# 	python fetchbus.py <MTA_KEY> <BUS_LINE> <BUS_DIRECTION> <DURATION>
+# 	python fetchbus.py <MTA_KEY> <BUS_LINE> <DURATION>
 ##############################
 
 # initialize
@@ -24,6 +24,7 @@ import matplotlib.pyplot as plt
 
 import time
 import calendar
+import collections
 from datetime import datetime
 
 try:
@@ -31,41 +32,69 @@ try:
 except ImportError:
     import urllib.request as urllib
 
+# function for dictionary flattening
+# code by Imran adopted and modified from
+# https://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
+def flatten(d, parent_key='', sep='_'):
+    """
+    Flatten the data of a nested dictionary.
+    
+    PARAMETERS
+    ----------
+    d: dictionary
+        a nested dictionary
+        
+    RETURNS
+    -------
+    dict(items): dictionary
+        a dictionary with all items unpacked from their nests
+    """
+    items = []
+    for k, v in d.items():
+        new_key = k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
 # function for fetching bus data
-def bus_data(apikey, route, direction=0, duration=5):
+def bus_data(apikey, route, duration=5):
     """
     Fetch MTA real-time bus location data for specified route and direction
     in a given duration.
 
-    ARGUMENTS
+    PARAMETERS
     ----------
-    apikey: API key for MTA data
-    route: route reference (e.g., B54)
-    direction: 0 or 1
-    duration: minutes of data to fetch (by 30-second intervals)
-    
-    RETURN
-    ----------
-    - a dataframe of vehicle location data
-    - a csv file containing the same data
+    apikey: string
+        API key for MTA data
+    route: string
+        route reference (e.g., B54)
+    duration: integer
+        minutes of data to fetch (by 30-second intervals)
+        
+    RETURNS
+    -------
+    df: pd.DataFrame
+        a dataframe of all SIRI variables for real-time bus trajectories
+    filename.csv: csv
+        a csv file containing the same data saved at local folder
     """
     
+    # name the output csv file
     ts = datetime.now()
     dow = calendar.day_name[ts.weekday()][:3]
-    ts_date = str(ts.year) + str(ts.month) + str(ts.day)
-    ts_time = str(ts.hour) + str(ts.minute) + str(ts.second)
-    filename = '%s-%s-%s-%s-%s-%s.csv'%(route, direction, dow, ts_date, ts_time, duration)
+    filename = '%s-%s-%s-%s.csv'%(route, dow, ts.strftime('%y%m%d-%H%M%S'), duration)
     
+    # set up parameters
+    t_elapsed = 0 # timer
     duration = int(duration) * 60 # minutes to seconds
-    t_elapsed = 0
+    url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(apikey, route)
+    df = pd.DataFrame() # empty dataframe
     
-    url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s&DirectionRef=%s"%(apikey, route, direction)
-    
-    df = pd.DataFrame(columns=['VehicleRef', 'Latitude', 'Longitude',
-                               'StopName', 'StopStatus', 'VehDistAlongRoute'])
+    # main block for fetching data
     while True:
-        
-        # fetch data
+        # fetch data through MTA API
         response = urllib.urlopen(url)
         data = response.read().decode("utf-8")
         data = json.loads(data)
@@ -73,63 +102,35 @@ def bus_data(apikey, route, direction=0, duration=5):
         # check if bus route exists
         try:
             data2 = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+        # print error if bus route not found
         except:
             error = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['ErrorCondition']
             print(error['Description'])
             sys.exit()
 
-        # parse route info: line number, active vehicles
-        print("Time Elapsed: " + str(t_elapsed/60) + " min(s)")
+        # print info of the current query request
+        print("\nTime Elapsed: " + str(t_elapsed/60) + " min(s)")
         print("Bus Line: " + route)
         print("Number of Active Buses: " + str(len(data2)))
 
-        # parse info
-        vrf = []
-        lat = []
-        lng = []
-        stop = []
-        status = []
-        vdars = []
-        tiempos = []
-
-        # parse vehicle info: location (lat, long)
+        # parse the data of each active vehicle
         for i, v in enumerate(data2):
-            # parse time
-            tiempo = v['RecordedAtTime']
-            # parse location
-            vr = v['MonitoredVehicleJourney']['VehicleRef'].split("_")[1]
-            lt = v['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-            lg = v['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-            vrf.append(vr)
-            lat.append(lt)
-            lng.append(lg)
+            dict1 = flatten(v['MonitoredVehicleJourney'])
+            dict1['RecordedAtTime'] = v['RecordedAtTime']
+            dict1['SituationSimpleRef'] = dict1['SituationRef'][0]['SituationSimpleRef']
+            dict1.pop('SituationRef')
+            dict1.pop('OnwardCall')
+            
+            # print info of the vehicle
+            print("Bus %s (#%s) is at latitude %s and longitude %s"%(i+1, dict1['VehicleRef'], dict1['Latitude'], dict1['Longitude']))
 
-            # parse next stop info
-            if 'OnwardCall' in v['MonitoredVehicleJourney']['OnwardCalls']:
-                s = v['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]
-                stt = s['Extensions']['Distances']['PresentableDistance']
-                stp = s['StopPointName']
-                cdar = s['Extensions']['Distances']['CallDistanceAlongRoute']
-                dfc = s['Extensions']['Distances']['DistanceFromCall']
-                vdar = cdar - dfc
-            else:
-                stt = 'N/A'
-                stp = 'N/A'
-            status.append(stt)
-            stop.append(stp)
-            vdars.append(vdar)
-            tiempos.append(tiempo)
-
-            # print info
-            print("Bus %s (#%s) is at latitude %s and longitude %s"%(i+1, vr, lt, lg))
-
-        # write data to dictionary, indexing from 1
-        d = {'VehicleRef': vrf, 'Latitude': lat, 'Longitude': lng,
-             'StopName': stop, 'StopStatus': status, 'VehDistAlongRoute': vdars,
-             'RecordedAtTime': tiempos}
-        df = pd.concat([df, pd.DataFrame(data=d)])        
-        df.to_csv(filename)
+            # write data to dictionary
+            df = pd.concat([df, pd.DataFrame(dict1, index=[i])])
         
+        # write/update data to csv
+        df.to_csv(filename)
+
+        # check and update timer
         if t_elapsed < duration:
             t_elapsed += 30
             time.sleep(30)
@@ -141,20 +142,26 @@ def plot_tsd(df, start_min=None, end_min=None, save=False, fname='TSD'):
     """
     Plot the time-space diagram for a given dataframe containing
     real-time MTA bus data (as generated from fetchbus.py)
-    
-    ARGUMENTS
+
+    PARAMETERS
     ----------
-    df: input dataframe containing required columns for plotting time-space diagram
-    start_min: plot from this given minute (time elapsed)
-    end_min: plot until this given minute (time elapsed)
-    save: save TSD to .png at current directory
-    fname: assign a filename for saved TSD (otherwise may be overwritten)
-    
-    RETURN
-    ----------
-    - fig
-    - ax
-    - a saved TSD .png file (optional)
+    df: pd.DataFrame
+        Input dataframe containing required columns for plotting time-space diagram.
+    start_min: numeric
+        Plot from this given minute (time elapsed).
+    end_min: numeric
+        Plot until this given minute (time elapsed).
+    save: boolean
+        Save TSD to .png at current directory.
+    fname: string
+        Assign a filename for saved TSD (otherwise may be overwritten).
+        
+    RETURNS
+    -------
+    fig:
+    ax:
+    filename.png: png
+        a saved TSD file (optional)
     """
     # determine time interval to be plotted
     try:
@@ -167,22 +174,31 @@ def plot_tsd(df, start_min=None, end_min=None, save=False, fname='TSD'):
     # convert time format
     df['RecordedAtTime'] = pd.to_datetime(df['RecordedAtTime'])
 
-    # plot
+    # plot figure
     fig = plt.figure(figsize=(12,8))
     ax = fig.add_subplot(111)
     
+    # calculate vehicle distance along the route
+    df['VehDistAlongRoute'] = df['CallDistanceAlongRoute'] - df['DistanceFromCall']
+    
+    # plot the trajectory for each vehicle
     for i, v in enumerate(df['VehicleRef'].unique()):
+        # subset data for single vehicle
         veh_df = df[df['VehicleRef'] == v]
+        # subset within specified time window
         veh_df = veh_df.iloc[s:e,:]
         
         ax.plot(veh_df['RecordedAtTime'], veh_df['VehDistAlongRoute'], marker='.')
-        ax.annotate('%s'%v, (list(veh_df['RecordedAtTime'])[0],list(veh_df['VehDistAlongRoute'])[0]))
+        ax.annotate('%s'%v.split("_")[1], (list(veh_df['RecordedAtTime'])[0],list(veh_df['VehDistAlongRoute'])[0]))
         
-        ax.set_xlabel("time (sec)", fontsize=14)
+        ax.grid()
+        ax.set_xlabel("time", fontsize=14)
         ax.set_ylabel("distance along route (m)", fontsize=14)
         ax.set_title("Time-space Diagram", fontsize=18)
     
     plt.tight_layout()
+    
+    # save figure locally if specified
     if save:
         plt.savefig("%s.png"%(fname), dpi=300)
     else:
@@ -193,9 +209,17 @@ def plot_tsd(df, start_min=None, end_min=None, save=False, fname='TSD'):
 
 # check input args
 if __name__ == '__main__':
-    if not len(sys.argv) == 5:
-        print ("Invalid number of arguments. Run as: python show_bus_locations_ywc249.py <MTA_KEY> <BUS_LINE>")
+    if not len(sys.argv) == 4:
+        print ("Invalid number of arguments. Run as: python fetchbus.py <MTA_KEY> <BUS_LINE> <DURATION>")
         sys.exit()
 
     # read args and fetch data
-    df = bus_data(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    df = bus_data(sys.argv[1], sys.argv[2], sys.argv[3])
+
+    # # name the output png file (!!!SHOULD UPDATE THIS PART!!!)
+    # ts = datetime.now()
+    # dow = calendar.day_name[ts.weekday()][:3]
+    # filename = '%s-%s-%s-%s'%(sys.argv[2], dow, ts.strftime('%y%m%d-%H%M%S'), sys.argv[3])
+
+    # # plot time-space diagram
+    # plot_tsd(df, save=True, fname=filename)
